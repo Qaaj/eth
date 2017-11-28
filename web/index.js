0c1457f9cb74443d3ad3311c411f4e1e530874f0
Jan <grasmaaier45@gmail.com>
2017-11-28 14:36:21 +0100

    Created pug view for block information

--- web/index.js
+++ web/index.js
@@ -1,7 +1,5 @@
 const Koa = require('koa');
-const app = new Koa();
 const Pug = require ('koa-pug');
-const moment = require('moment');
 const redis = require("redis");
 
 // const client = redis.createClient({host : 'redis'});
@@ -10,42 +8,14 @@ const sub = redis.createClient({host : 'redis'});
 
 sub.subscribe("web3");
 
-new Pug({
-  viewPath: './views',
-  app: app
-});
+const router = require('./routes')(pub,sub);
+const app = new Koa();
 
-app.use(async ctx => {
-  const coinbase = await doRequest('coinbase');
-  const blockheight = await doRequest('blockheight')
-  const blocks = [];
-  for (i = blockheight; (i > -1  && i > blockheight-5); i--) {
-    const info = JSON.parse(await doRequest('blockinfo', i));
-    console.log(info.timestamp);
-    blocks.push({
-      blockheight: i,
-      hash: info.hash,
-      timestamp: info.timestamp == 0 ? 'Genesis' : moment.unix(info.timestamp).fromNow(),
-      tx: info.transactions.length //await doRequest('blocktxcount', i)
-    });
-  }
-  ctx.render('index', { coinbase, blocks });
-});
+new Pug({ viewPath: './views',  app: app });
 
-const doRequest = (type,data = null) => new Promise((ok) => {
-  const rand = 'MSG_' + Math.round(Math.random()*100000000000);
-  sub.subscribe("web3");
-  sub.on("message", function (channel, message) {
-    const msg = message.split('_______');
-    const command = msg[0];
-    const data = msg[1];
-    if(command === rand) {
-      sub.unsubscribe();
-      ok(data)
-    }
-  });
-  pub.publish('web3', `${type}_______${data}_______${rand}`)
-});
+app
+  .use(router.routes())
+
 
 app.listen(3001);
 console.log("Listening on 3001");
---
+++ web/routes/index.js
@@ -0,0 +1,48 @@
+const Router = require('koa-router');
+const moment = require('moment');
+
+
+module.exports = (pub, sub) => {
+
+
+  const doRequest = (type,data = null) => new Promise((ok) => {
+    const rand = 'MSG_' + Math.round(Math.random()*100000000000);
+    sub.subscribe("web3");
+    sub.on("message", function (channel, message) {
+      const msg = message.split('_______');
+      const command = msg[0];
+      const data = msg[1];
+      if(command === rand) {
+        sub.unsubscribe();
+        ok(data)
+      }
+    });
+    pub.publish('web3', `${type}_______${data}_______${rand}`)
+  });
+
+
+  const router = new Router();
+
+  router.get('/', async (ctx) => {
+    const coinbase = await doRequest('coinbase');
+    const blockheight = await doRequest('blockheight')
+    const blocks = [];
+    for (i = blockheight; (i > -1 && i > blockheight - 5); i--) {
+      const info = JSON.parse(await doRequest('blockinfo', i));
+      blocks.push({
+        blockheight: i,
+        hash: info.hash,
+        timestamp: info.timestamp == 0 ? 'Genesis' : moment.unix(info.timestamp).fromNow(),
+        tx: info.transactions.length //await doRequest('blocktxcount', i)
+      });
+    }
+    ctx.render('index', {coinbase, blocks});
+  });
+
+  router.get('/block/:hash', (ctx) => {
+    console.log(ctx.params.hash)
+    ctx.render('block', {hash: ctx.params.hash});
+  });
+
+  return router;
+};
