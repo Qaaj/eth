c13658ea1f1cc12a90d6c6d9b54982c862eacf2c
Jan <grasmaaier45@gmail.com>
2018-02-02 13:47:28 +0100

    Updated Amazon config with custom network ID for geth

--- amazon_start.sh
+++ amazon_start.sh
@@ -2,7 +2,7 @@
 
 docker pull mordhau/eth-web
 docker pull mordhau/eth-data
-docker pull mordhau/ethereum-geth-dev
+docker pull mordhau/ethereum-geth-dev:144
 
 docker-compose -f docker-aws.yml up -d
 
